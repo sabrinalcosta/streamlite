8552f9bb0801421ec446de61e56c7081a95abae9
sabrinalcosta <slc@academico.ufpb.br>
2026-07-30 11:23:58 -0300

    Add Streamlit components and status messages

--- olamundo.py
+++ olamundo.py
@@ -2,3 +2,36 @@ import streamlit as st
 
 #imprimindo uma mensagem na tela
 st.write('Vamos aprender streamlit juntos')
+
+#usando várias formatações de texto
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
+st.markdown("Este é texto")
+st.caption("Esta é a a legenda")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+#criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+#mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
