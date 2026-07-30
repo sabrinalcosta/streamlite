0d86769d2b130081e74f3f3e965bf2ab932f26a4
sabrinalcosta <slc@academico.ufpb.br>
2026-07-30 11:24:47 -0300

    Add Streamlit app for displaying quilombola communities

--- olamundo.py
+++ olamundo.py
@@ -35,3 +35,57 @@ st.success("Você conseguiu!")
 st.error("Erro!")
 st.warning("Advertência")
 st.info("Esta é uma informação")
+
+#criando uma aplicação simples
+import streamlit as st
+#import no requirements.txt
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+#posso trocar estes dados por dados de tabelas reais!
+
+st.write("Criando uma tabela!")
+#tabelas interativas
+st.write(df)
+#inserindo um selectbox
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+#O formato de print é diferente de outras versões
+#de Python
+st.write('Você selecionou: ', opcao)
+#como filtrar os dados pelo nome?
+
+#filtrando os dados pelo nome
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+dadosFiltrados
+
+import streamlit as st
+import pandas as pd
+#import no requirements.txt
+
+st.title('Localização das comunidades quilombolas (2022)')
+
+#preparando o dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
+#apresentar o dataframe
+#https://www.ibge.gov.br/geociencias/organizacao-do-territorio/estrutura-territorial/27385-localidades.html?=&t=downloads
+#apresentar o dicionario de dados (disponível em Localizado em "Downloads > Localidades quilombolas > Arquivos vetoriais > dicionario LQs")
+#informar que dados são remanunfaturados (vamos aproveitar dados de pesquisas já em andamento)
+
+#informar que precisa limpar os dados
+df.info()
+
+#criando um slider
+numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
+st.write(df.head(numero))
+
+#visualizando o número de comunidades por estado
+df['NM_UF'].value_counts()
+
+#criando um gráfico de barras
+st.bar_chart(df['NM_UF'].value_counts())
