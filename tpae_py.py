90cd8dfaea43d9b5baf42bc832462cdf579936a0
sabrinalcosta <slc@academico.ufpb.br>
2026-08-04 10:30:05 -0300

    Remove pip install command for streamlit

--- tpae_py.py
+++ tpae_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1GTHdFANmO4OmqHkR1R_DOkvcW1fa1Ft9
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
